fa2993e6b23f1b679d61b7cc5db12f508bbdee51
harunmoyal <harunmoyal.hm@gmail.com>
2023-03-04 00:23:29 +0530

    nothing

---
+++ basic java script.js
@@ -0,0 +1,36 @@
+console.log("operators in java scripts");
+  let a = 50;
+  let b = 60;
+  console.log("Arthmatic operators");
+  console.log('a+b', a+b);
+  console.log('a-b', a-b);
+  console.log('a/b', a/b);
+  console.log('a*b', a*b);
+  console.log('a%b', a%b);
+  console.log('10**10', 10**10);
+  console.log("a++",a);
+  //first print a value after incerminet
+  console.log(a);
+  console.log("++a",++a);
+  //first incriment the value after print
+  console.log("a--",a--);
+    //first print the value after decrising value
+  console.log(a);
+  console.log("--b",--b);
+  //fir st decriment the value after print
+console.log("Assigment Operators");
+let ar = 10;
+ar +=5
+console.log("ar+=5",ar);
+ //comparison operator
+   let num = 20;
+   let num2 = "20";
+   console.log("num == num2 is",num==num2);
+   console.log("num!=num2 is",num!=num2);
+   console.log("num===num2 is",num===num2);
+   //=== is not equal for string to let or number equal value and type
+   //logical operators
+   let lo = 10;
+   let lo1 = 15;
+   console.log(lo<lo1 && lo==11 );
+   console.log(lo>lo1|| lo==10);
